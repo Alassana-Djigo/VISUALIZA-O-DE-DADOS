71197c9ad89a2eb38e9c0ced564e5c6956720089
Alassana-Djigo <alassana7djigo@gmail.com>
2025-02-26 15:19:02 +0100

    Resolvido

---
+++ Analizando_dados_em_arquivos_json/country_code.py
@@ -0,0 +1,8 @@
+from pygal.maps.world import COUNTRIES
+
+def get_country_code(country_name):
+	"""Devolve o codigo de duas letras do pygal para um país dado seu nome """
+	for code, name in COUNTRIES.items():
+		if name == country_name:
+			return code
+	return None
